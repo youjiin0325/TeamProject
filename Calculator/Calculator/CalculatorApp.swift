07f6539c1cb731589ae85625ddcef682664321ef
youjiin0325 <youjiin0325@gmail.com>
2023-11-09 22:09:03 +0900

    Feat: Create .gitignore

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by YOU on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
